aafbd0d6cf3470bfff2c3f230246adddeee5d778
Tomer Orian <tomero@h-MacBook-Pro-sl-TomerO.local>
2018-06-11 10:20:27 +0300

    respond only to messages for the bot

--- src/bot.js
+++ src/bot.js
@@ -9,12 +9,14 @@ rtm.on('message', (event) => {
     // For structure of `event`, see https://api.slack.com/events/message
 
     // Skip messages that are from a bot or my own user ID
-    if ( (event.subtype && event.subtype === 'bot_message') ||
-        (!event.subtype && event.user === rtm.activeUserId) ) {
+    // Skip messages not for the bot
+    if ((event.subtype && event.subtype === 'bot_message') ||
+        (!event.subtype && event.user === rtm.activeUserId) ||
+        !event.text.includes(`<@${rtm.activeUserId}>`)) {
         return;
     }
 
-    rtm.sendMessage('I am here an I am alive', event.channel)
+    rtm.sendMessage(`Hi <@${event.user}>! I am here an I am alive`, event.channel)
         .then((res) => {
             // `res` contains information about the posted message
             console.log('Message sent: ', res.ts);
